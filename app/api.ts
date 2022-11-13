9f2b9ee20f1d72584342e8c619846fd39e30192d
taylor-ben <taylor.ben.dev@gmail.com>
2022-11-13 18:50:57 +0200

    cleanup

--- app/api.ts
+++ app/api.ts
@@ -11,22 +11,12 @@ async function fetchInterceptor<T>(
   return res.json()
 }
 
-// export async function fetchPosts(searchParams: SearchParams) {
-//   console.log('searchParams:', searchParams)
-//   return fetchInterceptor<PostsResponse>('posts', {
-//     body: JSON.stringify(searchParams),
-//     method: 'POST',
-//     // next: { revalidate: 60 },
-//     cache: 'no-cache',
-//   })
-// }
 export async function fetchPosts(searchParams: SearchParams) {
   return fetchInterceptor<PostsResponse>(
     'posts',
     {
       method: 'GET',
       next: { revalidate: 60 },
-      cache: 'no-cache',
     },
     searchParams
   )
